dbc0f1e99d83fccfdf37f1db1599a693ad1dd419
Barbara Litwin <litwinbasia85@gmail.com>
2025-11-13 19:31:44 +0100

    fixed

--- CoreLayer/Factory.cs
+++ CoreLayer/Factory.cs
@@ -1,8 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Interactions;
 
 namespace CoreLayer.WebDriver
 {
